a065fddc2152d4b38c853ee6e019bd382ba10fc8
John T. Wodder II <git@varonathe.org>
2021-05-28 10:00:10 -0400

    Comments on regexen

--- src/ghrepo/__init__.py
+++ src/ghrepo/__init__.py
@@ -17,12 +17,28 @@ from typing import Callable, NamedTuple, Optional, Union
 
 AnyPath = Union[str, bytes, "PathLike[str]", "PathLike[bytes]"]
 
+#: Regular expression for a valid GitHub username or organization name.  As of
+#: 2017-07-23, trying to sign up to GitHub with an invalid username or create
+#: an organization with an invalid name gives the message "Username may only
+#: contain alphanumeric characters or single hyphens, and cannot begin or end
+#: with a hyphen".  Additionally, trying to create a user named "none" (case
+#: insensitive) gives the message "Username name 'none' is a reserved word."
+#:
+#: Unfortunately, there are a number of users who made accounts before the
+#: current name restrictions were put in place, and so this regex also needs to
+#: accept names that contain underscores, contain multiple consecutive hyphens,
+#: begin with a hyphen, and/or end with a hyphen.
 GH_USER_RGX = r"(?![Nn][Oo][Nn][Ee]($|[^-A-Za-z0-9]))[-_A-Za-z0-9]+"
+
+#: Regular expression for a valid GitHub repository name.  Testing as of
+#: 2017-05-21 indicates that repository names can be composed of alphanumeric
+#: ASCII characters, hyphens, periods, and/or underscores, with the names ``.``
+#: and ``..`` being reserved and names ending with ``.git`` forbidden.
 GH_REPO_RGX = r"(?:\.?[-A-Za-z0-9_][-A-Za-z0-9_.]*|\.\.[-A-Za-z0-9_.]+)(?<!\.git)"
 
 OWNER_REPO_RGX = re.compile(fr"(?:(?P<owner>{GH_USER_RGX})/)?(?P<name>{GH_REPO_RGX})")
 
-GITHUB_REMOTE_RGX = re.compile(
+GITHUB_REPO_RGX = re.compile(
     fr"""
     (?:
         (?:https?://)?(?:(?:www\.)?github\.com/|api\.github\.com/repos/)
@@ -71,7 +87,7 @@ class GHRepo(NamedTuple):
     @classmethod
     def parse_url(cls, url: str) -> "GHRepo":
         # cf. the giturlparse library
-        m = GITHUB_REMOTE_RGX.fullmatch(url)
+        m = GITHUB_REPO_RGX.fullmatch(url)
         if m:
             return cls(owner=m["owner"], name=m["name"])
         else:
